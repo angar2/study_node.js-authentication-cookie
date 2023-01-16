020ba88f9da28227d63b64d00977e33385f13b51
앵가리 <>
2023-01-16 19:10:23 +0900

    Cookie 활용 연습: Cookie 생성: Permanent

--- cookie.js
+++ cookie.js
@@ -1,9 +1,12 @@
 const http = require('http');
 const cookie = require('cookie');
 http.createServer((req, res) => {
-    // res.writeHead(200, {
-    //     'Set-cookie': ['yummy_cookie=choco', 'tasty_cookie=strawbarry']
-    // });
+    res.writeHead(200, {
+        'Set-cookie': [
+            'yummy_cookie=choco', 
+            'tasty_cookie=strawbarry',
+            `permanent=cookie; Max-age=${60*60*24*365}`]
+    });
     var cookies = {};
     if(req.headers.cookie !== undefined){
         cookies = cookie.parse(req.headers.cookie);
